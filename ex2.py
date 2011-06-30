c9c85d670e161c1df461a8085001a33be4ff5a23
aa2858 <aa2858@gmail.com>
2011-06-30 17:31:18 -0400

    adding exercise files

---
+++ ex2.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        ex2.py
+# Purpose:
+#
+# Author:      AAliu
+#
+# Created:     29/06/2011
+# Copyright:   (c) AAliu 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+# A comment, this is so you can read your program later
+# Anything after # is ignored by python
+
+print "I could have code like this."# and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+
+print "This will run."
+
